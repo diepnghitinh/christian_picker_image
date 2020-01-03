36413b77d8053f4feae12d4eb2db8928aa67fab7
Hon Vi Dan <dan.honvi@gmail.com>
2020-01-03 15:15:37 +0700

    - remove duplicate code

--- android/src/main/java/com/imagepicker/features/camera/OnImageReadyListener.java
+++
@@ -1,9 +0,0 @@
-package com.imagepicker.features.camera;
-
-import com.imagepicker.model.Image;
-
-import java.util.List;
-
-public interface OnImageReadyListener {
-    void onImageReady(List<Image> image);
-}
