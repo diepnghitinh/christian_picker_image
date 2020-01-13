72c21f7c564653ed09a2743dc4ce436629eb4a5b
nguyen phuc nguyen <heycrmvn@gmail.com>
2020-01-13 22:08:22 +0700

    0.1.4

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Volumes/Data/flutter_1.12.13"
+export "FLUTTER_ROOT=/Volumes/Data/flutter"
 export "FLUTTER_APPLICATION_PATH=/Volumes/Data/Github/christian_picker_image/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Volumes/Data/flutter_1.12.13/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Volumes/Data/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
